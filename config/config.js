8b27e394d8755f5abb2fa8efa711f8f114ee4ee1
Andre Cronje <andre@mfin.io>
2018-05-29 12:43:56 +0200

    Live, do not store details like this.
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var config = {
 
   host: 'cryptocurvepostgres.cfytsjuclasa.us-east-2.rds.amazonaws.com',
-  database: 'CryptoCurveDev',
+  database: 'CryptoCurveLive',
   user: 'whitelistpresale',
   password: 'wh1t3l1st!@#JKL!@#',
 }
